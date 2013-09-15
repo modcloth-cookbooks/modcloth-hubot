d317ab28b22a5705a64fb5c4035f2a61e6a473a4
Dan Buch <d.buch@modcloth.com>
2013-09-15 17:19:19 -0400

    Bumping version by a patch after adding tag for 0.1.0

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'github+modcloth-hubot-cookbook@modcloth.com'
 license          'MIT'
 description      'Installs/Configures modcloth-hubot'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 supports 'smartos'
 supports 'ubuntu', '>= 10.04'
